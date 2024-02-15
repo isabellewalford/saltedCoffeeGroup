aebca662b0623541c2bf05bda79ed5f33502f448
HRW221 <hrw221@exeter.ac.uk>
2024-02-15 16:48:23 +0000

    Created view stubs

--- sustainability/urls.py
+++ sustainability/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("login/", views.login, name="login"),
+    path("leaderboard/", views.leaderboard, name="leaderboard"),
+    path("user/", views.user, name="user"),
+    path("user/photo/", views.photo, name="photo"),
+    path("user/collection/", views.collection, name="collection"),
 ]
--- sustainability/views.py
+++ sustainability/views.py
@@ -6,3 +6,18 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Sustainability index")
+
+def login(request):
+    return HttpResponse("login")
+
+def leaderboard(request):
+    return HttpResponse("leaderboard")
+
+def user(request):
+    return HttpResponse("user account")
+
+def photo(request):
+    return HttpResponse("taking photo")
+
+def collection(request):
+    return HttpResponse("card collection")
