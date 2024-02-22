d4f2d53d1c448b6299ddc9f9c212e8b8be0d2c87
isabellewalford <issywalf@gmail.com>
2024-02-22 22:03:35 +0000

    registration app

--- sustainability/forms.py
+++ sustainability/forms.py
@@ -5,14 +5,6 @@ from django.contrib.auth.models import User
 from sustainability.models import PlantOfTheDay
 
 
-class SignUpForm(UserCreationForm):
-    email = forms.EmailField(max_length=150, help_text='Enter a valid email address.')
-
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'password1', 'password2']
-
-
 class PlantOfTheDayForm(forms.ModelForm):
     class Meta:
         model = PlantOfTheDay
